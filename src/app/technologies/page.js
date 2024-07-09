37e51dc9c590a0f1da1cace7ce8149ef33a7bba5
Zeeshan2335 <94827220+Zeeshan2335@users.noreply.github.com>
2024-07-09 18:45:37 +0530

    feat(): add progress bar with card

--- src/app/technologies/page.js
+++ src/app/technologies/page.js
@@ -1,8 +1,25 @@
+"use client";
+import TechCard from "@/components/TechCard";
+
 export default function Technologies() {
-    return (
-      <div className="min-h-screen">
-        <h1>Technologies</h1>
+  return (
+    <div className="min-h-screen ">
+      <h1>Technologies</h1>
+      <div className="md:flex-row flex flex-col  justify-center w-full  items-center">
+        <div className=" w-4/5  md:w-1/4">
+          <TechCard />
+        </div>
+        <div className="   w-4/5  md:w-1/4">
+          <TechCard />
+        </div>
+        <div className="   w-4/5  md:w-1/4">
+          <TechCard />
+        </div>
+        <div className="   w-4/5  md:w-1/4">
+          <TechCard />
+        </div>
+       
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
